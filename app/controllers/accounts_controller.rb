e5921e885e028650b25a8560395ef0447018bf64
kjacky <holy_dragon_88@hotmail.com>
2010-03-04 11:00:42 -0800

    Added password recovery.
    Your password will be emailed to you.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,5 @@
 class AccountsController < ApplicationController
-    before_filter :login_required, :except => [:login, :process_login, :index, :logout]
+    before_filter :login_required, :except => [:login, :process_login, :index, :logout, :forgot_password]
     
     def login
       @user = Volunteer.new
@@ -32,5 +32,17 @@ class AccountsController < ApplicationController
     def save_account
       flash[:message] = '[[todo, validate data and save]] Your account has been updated.'
       redirect_to :action => :my_account
+    end
+    
+    def forgot_password
+      if request.post?
+        user = Volunteer.find_by_email(params[:user][:email])
+        if user == nil
+          flash[:message] = "Sorry we could not find an account associated with that email. Please register for an account first."
+        else
+          Emailer.deliver_forgot_password(user.email, user.password)
+          flash[:message] = "Your request has been received and an email will sent to you shortly.  If you do not find it in your inbox, please check your spam folders."
+        end
+      end
     end
 end
