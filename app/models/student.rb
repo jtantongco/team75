fce6f8b79dc04cb7f11a85fbc54a75f73eb9058b
unknown <Vince@.(none)>
2010-03-04 18:58:36 -0800

    Finishing touches on form validation.  Only Date left now >:(

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,6 @@ class Student < ActiveRecord::Base
   
   validates_presence_of     :student_id,  :faculty,     :program
   validates_numericality_of :student_id,  :only_integer => true 
+  validates_uniqueness_of :student_id,
+  					      :message => 'This student number is already registered!  Please enter another student number.'
 end
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -4,7 +4,17 @@ class Volunteer < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email, :password, :birthday, :address, :location, :postal_code, :province, :emrg_contact_name, :emrg_contact_relationship 
   validates_associated :student, :volunteer_extra
 
+  validates_format_of :email,
+					  :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+					  :message => 'Email address must be valid'
+				
+  validates_format_of :postal_code,
+					  :with => /^[a-zA-Z]\d[a-zA-Z]\d[a-zA-Z]\d$/,
+					  :message => 'Postal code must be valid'
 
+  validates_uniqueness_of :email,
+  						  :message => 'This email address has already been used'
+  						 
   has_one :student,         :dependent => :destroy
   has_one :volunteer_extra, :dependent => :destroy
 
@@ -13,4 +23,5 @@ class Volunteer < ActiveRecord::Base
   def self.authenticate(user_info)
     find_by_email_and_password(user_info[:email], user_info[:password])
   end
+  
 end
