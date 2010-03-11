e6d6a6e8f436142645bfab62f1a2162afb7f178e
Anders Lisspers <anli8013@interchange.ubc.ca>
2010-03-11 14:39:22 -0800

    Created self-report controller and moved stuff there.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -78,23 +78,7 @@ class AccountsController < ApplicationController
       end
     end
   end
-
-  def report_hours
-    @report = SelfReport.new
-
-    if request.post?
-      @report = SelfReport.new(params[:self_report])
-      @report.volunteer_id = @user.v_id
-
-      if @report.save
-        flash[:message] = '[todo: redirect to report list] Your hour report has been saved. It will be verified by the supervisor.'
-        redirect_to :action => :my_account
-      else
-        render :action => :report_hours
-      end
-    end
-  end
-
+  
   def forgot_password
     if request.post?
       user = Volunteer.find_by_email(params[:user][:email])
