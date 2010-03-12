7b7fce1ac2c191b8cdf3fb3033a73fde2167ae2c
Anders Lisspers <anli8013@interchange.ubc.ca>
2010-03-12 08:36:00 -0800

    Editing self reports now possible

--- app/controllers/self_report_controller.rb
+++ app/controllers/self_report_controller.rb
@@ -16,6 +16,23 @@ class SelfReportController < ApplicationController
     end
   end
   
+  def edit_report
+    begin
+      @report = @user.self_report.find(params[:id])
+    rescue
+      flash[:error] = 'Report not found'
+      redirect_to :action => :index
+    end
+    
+    if request.put? 
+      if @report.update_attributes(params[:self_report])
+        flash[:message] = 'Your report has been updated.'
+        redirect_to :action => :index
+      end
+      logger.info 'POST'
+    end
+  end
+  
   # Lists the volunteers reported hours
   def index
     @reports = @user.self_report
--- app/models/self_report.rb
+++ app/models/self_report.rb
@@ -4,9 +4,10 @@ class SelfReport < ActiveRecord::Base
   belongs_to :project
   belongs_to :supervisor
   
-  validates_presence_of :project_id, :supervisor_id, :start_time, :end_time
+  validates_presence_of :project_id, :supervisor_id, :date, :start_time, :end_time
   validate :dates_are_not_in_the_future, :end_date_is_after_start_date
-  
+
+
   def dates_are_not_in_the_future
     start_date  =  Time.parse(date.to_s + " " + start_time.hour.to_s + ":" + start_time.min.to_s)
     end_date  =  Time.parse(date.to_s + " " + end_time.hour.to_s + ":" + end_time.min.to_s)
@@ -16,14 +17,21 @@ class SelfReport < ActiveRecord::Base
   end
   
   def end_date_is_after_start_date
-    errors.add(:end_time, "can't be before start time") if start_time  > end_time
+    start_date  =  Time.parse(date.to_s + " " + start_time.hour.to_s + ":" + start_time.min.to_s)
+    end_date  =  Time.parse(date.to_s + " " + end_time.hour.to_s + ":" + end_time.min.to_s)
+
+    errors.add(:end_time, "can't be before start time") if start_date  > end_date
   end
   
   def hours
     total = (end_time - start_time) / 1.hour
     hours = total.floor
     minutes = (total - hours).minutes.round
-    "#{hours}:#{minutes.to_s.ljust(2, 0.to_s)}"
+    if minutes > 0
+      "#{hours} h #{minutes.to_s.ljust(2, 0.to_s)} min"
+    else
+      "#{hours} h"
+    end
   end
   
   # todo: Remove these when project and supervisor models are created
