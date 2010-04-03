809fe4b4e02e7d43bd885c4363ee38a9c5ab9f24
unknown <Jason@.(none)>
2010-04-02 18:25:01 -0700

    exporter methods heads

---
+++ app/controllers/exporter_controller.rb
@@ -0,0 +1,117 @@
+class ExporterController < ApplicationController
+
+require 'fastercsv'
+require 'csv'
+
+
+
+
+def export_a_accounts
+end
+def import_a_accounts
+end
+def export_administrators
+end
+def import_administrators
+end
+
+def export_awarded_profiles
+end
+def import_awarded_profiles
+end
+def export_email_profile
+end
+def import_email_profile
+end
+def export_group_profile
+end
+def import_group_profile
+end
+def export_orientations
+end
+def import_orientations
+end
+def export_project_feedbacks
+end
+def import_project_feedbacks
+end
+def export_project_orientations
+end
+def import_project_orientations
+end 
+def export_projects
+end
+def import_reports
+end
+def export_reward_profiles
+end
+def import_reward_profiles
+end
+def export_reports
+end
+def import_reports
+end
+def export_reward_profiles
+end
+def import_rewards
+end
+def export_s_accounts
+end
+def import_s_accounts
+end
+def export_schema_info
+end
+def import_schema_organization
+end
+def export_schema_info
+end
+def import_organization
+end
+def export_self_reports
+end
+def import_self_reports
+end
+def export_students
+end
+def import_supervises
+end
+def import_students
+end
+def export_supervises
+end
+def import_volunteer_extras
+end
+def export_volunteer_extras
+end
+def import_volunteer_project_interests
+end
+def export_volunteer_project_interests
+end
+def import_volunteers
+end
+def export_volunteers
+end
+def import_volunteer_orientations
+end
+def export_volunteer_orientations
+end
+def import_volunteer_projects
+end
+def export_volunteer_projects
+end
+def backupdatabase
+end
+def backupdatabase
+end
+
+
+
+
+
+
+
+
+
+
+
+end
