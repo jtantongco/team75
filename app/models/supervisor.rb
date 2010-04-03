cc81cacdb09030013a1efa9c21fba8773f96047d
unknown <Jason@.(none)>
2010-04-02 18:30:05 -0700

    supervisor

--- app/models/supervisor.rb
+++ app/models/supervisor.rb
@@ -1,10 +1,20 @@
 class Supervisor < ActiveRecord::Base
-  set_primary_key "s_id"
-  
-  has_many :self_reports
-  
-  validates_presence_of :name, :login_name, :password, :address,
-    :phone_num, :email
-  
-  validates_confirmation_of :password
+set_primary_key "s_id"
+
+#has_many :supervises
+#has_many :self_reports
+#has_many:projects
+
+validates_presence_of :name, :login_name, :password, :address
+
+validates_format_of :email,
+:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+:message => 'must be valid'
+
+validates_uniqueness_of :email,
+   :message => 'address has already been used'
+
+validates_confirmation_of :email 
+validates_presence_of :email_confirmation
+validates_confirmation_of :password 
 end
